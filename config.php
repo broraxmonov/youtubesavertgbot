9217e120cae040e74597acefedc2336c4a8e53e6
broraxmonov <bro.raxmonov@gmail.com>
2024-11-20 22:06:18 +0500

    dsadsad

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <? session_start();
 
-const API_KEY = 'tgram api token botfatherdan oalsiz';
-const YOUTUBE_API_KEY = 'Youtubedan api key oling';
+const API_KEY = '7359518594:AAFaBbD8mv5GjhjAVrUwBWDGV39md_iFx_Q';
+const YOUTUBE_API_KEY = 'AIzaSyBQMjuhuLvFD0l5p2X_TFQ9M30eq7yCp5o';
 
 $system_pass = "123";
 $logging = true; //false
